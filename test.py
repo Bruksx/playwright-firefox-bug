e623005c7733a02e6b3245848a8e81fff828418b
Bruksx <jenake8@gmail.com>
2025-08-13 19:19:00 +0100

    first commit

---
+++ test.py
@@ -0,0 +1,14 @@
+from playwright.sync_api import sync_playwright
+
+FIREFOX_EXECUTABLE_PATH="/home/ubuntu/.cache/ms-playwright/firefox-1489/firefox/firefox"
+
+def main():
+    with sync_playwright() as p:
+        if FIREFOX_EXECUTABLE_PATH:
+            browser = p.firefox.launch(executable_path=FIREFOX_EXECUTABLE_PATH)
+        else:
+            browser = p.firefox.launch()
+        page = browser.new_page()
+        page.goto("https://www.flexjobs.com/", timeout=10000, wait_until="domcontentloaded")
+        page.query_selector(f'a[href="https://www.flexjobs.com/"]')
+        browser.close()
